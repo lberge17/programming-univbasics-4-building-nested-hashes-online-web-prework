f1b354c633d53e779af78e063a039b7357cf5e3c
Laura Berge <lauraeberge@gmail.com>
2019-08-28 17:46:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
     },
     :names => {
       :reading_railroad => {
-        "mortage value" => "$100"
+        "mortage_value" => "$100"
       },
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
